9c1910d3a28ead56143ca8168e38704a7e30ae2f
Chu Xuân Tình <chuxuantinhcs11@gmail.com>
2022-09-04 00:35:56 +0700

    Initial commit

---
+++ TEST_NET6_ADO_MODEL/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEST_NET6_ADO_MODEL.Models;
+
+
+namespace TEST_NET6_ADO_MODEL.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly Entities _DbContext = new Entities();
+        // GET: User
+        public ActionResult Index()
+        {
+            var emp = _DbContext.Users.ToList();
+            return View(emp);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            if (ModelState.IsValid)
+            {
+                User emp = new User();
+                emp.Id = Convert.ToInt32(formCollection["Id"]);
+                emp.UserName = formCollection["UserName"];
+                emp.PassWord = formCollection["PassWord"];
+                emp.FullName = formCollection["FullName"];
+                emp.Email = formCollection["Email"];
+                emp.PhoneNumber = formCollection["PhoneNumber"];
+                emp.Address = formCollection["Address"];
+                emp.Department = formCollection["Department"];
+                emp.Position = formCollection["Position"];
+
+                emp.RoleId = Convert.ToInt32(formCollection["RoleId"]);
+                emp.Status = Convert.ToInt32(formCollection["Status"]);
+               
+                _DbContext.Users.Add(emp);
+                _DbContext.SaveChanges();
+                RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ TEST_NET6_ADO_MODEL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEST_NET6_ADO_MODEL.MvcApplication" Language="C#" %>
